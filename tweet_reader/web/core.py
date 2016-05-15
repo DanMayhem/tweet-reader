24d1087308544d1e00157ac63963252facb23e2e
Dan May <danmay@gmail.com>
2016-05-15 10:54:28 -0400

    get landing page up running!

--- tweet_reader/web/core.py
+++ tweet_reader/web/core.py
@@ -1,11 +1,16 @@
 #!python
 
-#from flask_booststrap import Bootstrap
+from flask_bootstrap import Bootstrap
 
 from ..core import create_app as parent_create_app
-#from .static import bp as static_bp
+from .home import bp as home_bp
 #from .users import bp as users_bp
 
 def create_app():
   app = parent_create_app(__name__)
+
+  Bootstrap(app)
+
+  app.register_blueprint(home_bp)
+
   return app
--- tweet_reader/web/home.py
+++ tweet_reader/web/home.py
@@ -8,7 +8,7 @@ from flask.ext.login import login_required, current_user
 bp = flask.Blueprint("home", __name__)
 
 @bp.route("/")
-@bp.route("index.html")
-@bp.route("index")
+@bp.route("/index.html")
+@bp.route("/index")
 def index():
   return flask.render_template("landing.html")
