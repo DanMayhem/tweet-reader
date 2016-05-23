3850517dcde226ae35a14a77600f5a5ec74c08a7
Dan May <dan.may@salesforce.com>
2016-05-23 08:59:59 -0400

    fix js to write to form fields

--- tweet_reader/web/static/js/location.js
+++ tweet_reader/web/static/js/location.js
@@ -1,9 +1,11 @@
 function post_location(loc) {
-
-}
+  document.getElementById("latitude").value = loc.coords.latitude;
+  document.getElementById("longitude").value = loc.coords.longitude;
+  document.getElementById("radius").value = '250';
+};
 
 function request_location() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(post_location)
+    navigator.geolocation.getCurrentPosition(post_location);
   }
-}
+};
