b794d8535ef5319cf923eb3f8ef2c4ff16b0899c
Dan May <dan.may@salesforce.com>
2016-06-13 16:31:04 -0400

    add command to list all tweets in campaign

--- manager.py
+++ manager.py
@@ -7,6 +7,7 @@ import flask.ext.script
 
 from tweet_reader import mongo
 from tweet_reader.web import create_app
+from tweet_reader.campaigns import find_campaign
 
 app = create_app()
 
@@ -29,6 +30,15 @@ def list_camps():
     if 'radius' not in c:
       c['radius'] = 'n/a'
     print("{_id} @{owner}: '{search} [{latitude}, {longitude}, {radius}]'".format_map(c))
+
+@manager.command
+@manager.option('camp',help='campaign id')
+def show_camp(camp):
+  for tweet in mongo.db.tweets.find({'camp_key': camp}):
+    print("@{username}\n{text}\n".format(**tweet))
+
+
+
 
 if __name__=="__main__":
   manager.run()
