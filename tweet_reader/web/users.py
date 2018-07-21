71c09eb8602b14809403949c9764d1156fffd26b
Dan May <danmay@gmail.com>
2018-07-21 10:14:10 -0400

    ...

--- tweet_reader/web/users.py
+++ tweet_reader/web/users.py
@@ -32,7 +32,7 @@ def login():
 @login_required
 @bp.route('/logout')
 def logout():
-  flask.ext.login.logout_user()
+  flask_login.logout_user()
   flask.flash('Logged out.', 'success')
   return flask.redirect(flask.url_for('.index'))
 
@@ -50,7 +50,7 @@ def authorized():
     u.fetch_twitter_details()
     u.save()
 
-    flask.ext.login.login_user(u, remember=True)
+    flask_login.login_user(u, remember=True)
 
   return flask.redirect(flask.url_for('users.index'))
 
